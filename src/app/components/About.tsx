2cdd04670c2436d8abf1ff1674f4622eebfde1a5
Mahryousufzai <160025763+Mahryousufzai@users.noreply.github.com>
2024-11-15 02:42:46 +0500

    Fix: Escape single quote error in Contact.tsx

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -26,7 +26,7 @@ const About = () => {
       <h2 className={styles.heading}>About Me</h2>
       <p className={styles.description} data-aos="fade-down-left">
         I am a student at GIAIC, pursuing a course in Artificial Intelligence & Web 3.0 & Metaverse.
-        I'm passionate about technology and constantly learning new skills to stay up-to-date with the latest innovations.
+        I&apos;m passionate about technology and constantly learning new skills to stay up-to-date with the latest innovations.
       </p>
     </div>
   );
